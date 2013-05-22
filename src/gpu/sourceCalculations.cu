e3ebb20d9172585a0df969d9ea2b980f38e22014
atristandyer <bigblob202@gmail.com>
2013-05-22 16:03:56 -0400

    Finished bed slope source kernel

--- src/gpu/sourceCalculations.cu
+++ src/gpu/sourceCalculations.cu
@@ -1,4 +1,4 @@
-__global__ void bedSlopeSourceSolver(float *BedSlopeSource, float *UIntPts, float *BottomIntPts, int m, int n, float dx, float dy)
+__global__ void bedSlopeSourceSolver(float *BedSlopeSource, float *U, float *BottomIntPts, int m, int n, float dx, float dy)
 {
 	// Calculate the row and column of the thread within the thread block
 	int row = blockIdx.y * blockDim.y + threadIdx.y;
@@ -10,13 +10,25 @@ __global__ void bedSlopeSourceSolver(float *BedSlopeSource, float *UIntPts, floa
 		// Calculate index of this cell in the BedSlopeSource matrix
 		int slopeIndex = row*n*2 + col*2;
 		
-		// Calculate index of this cell in the UIntPts matrix
-		int uIndex = row*n*4*3 + col*4*3;
+		// Calculate index of this cell in the U matrix
+		int uIndex = row*n*3 + col*3;
 		
 		// Calculate index of this cell in the BottomIntPts matrix
 		int bottomIndex = row*(n+1)*2 + col*2;
 		
-		//// Calculate the water depth at the center of the cell
-		// Water depth 
+		// Calculate the water depth at the center of the cell
+		// Note: h = w - B;
+		//	 w is the water column height at the cell center measured from z = 0;
+		//	 B is the terrain elevation measured from z = 0, and is calculated as the average
+		//	   elevation of the center of two opposing edges (in this case, left and right)
+		float hCenter =  U[uIndex] - (BottomIntPts[bottomIndex+1] + BottomIntPts[bottomIndex + 1*2 + 1])/2.0f;
+		
+		// Calculate the slope of the terrain in both x- and y-directions
+		float slopeX = (BottomIntPts[bottomIndex + 3] - BottomIntPts[bottomIndex + 1]) / dx;
+		float slopeY = (BottomIntPts[bottomIndex + (n+1)*2] - BottomIntPts[bottomIndex]) / dy;
+		
+		// Calculate the bed slope source terms
+		BedSlopeSource[slopeIndex] = -9.81f * slopeX * hCenter;
+		BedSlopeSource[slopeIndex+1] = -9.81f * slopeY * hCenter;		 
 	}
 }
